26c78a6158c00d1d57bc46f308018eeb9270593f
cristian <Heavyblade@users.noreply.github.com>
2023-12-08 18:33:55 -0500

    Adding Repl interface

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"interpreter/repl"
+	"os"
+	"os/user"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is Monkey programming language!\n", user.Name)
+	fmt.Println("Feel free to type some commands")
+
+	repl.Start(os.Stdin, os.Stdout)
 }
